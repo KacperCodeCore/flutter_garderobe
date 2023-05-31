d3930737c19e9cdb7df82d6eaf231d98abed86f1
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-05-31 16:31:29 +0200

    +

--- flutter_application/lib/pages/clothes.dart
+++ flutter_application/lib/pages/clothes.dart
@@ -5,18 +5,23 @@ class UserClothes extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Clothes'),
-          Row(
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(Icons.abc),
-              Icon(Icons.abc),
+              Text('Clothes'),
+              Row(
+                children: [
+                  Icon(Icons.abc),
+                  Icon(Icons.abc),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
-    ));
+        ),
+        body: Container(
+          color: Colors.blue,
+          height: 400,
+          width: 400,
+        ));
   }
 }
