aff94b400336d641a83b4278be350244f72c3f7c
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-08-02 23:59:19 +0200

    button fix

--- flutter_application/lib/pages/page_element_creator/element_page_creator.dart
+++ flutter_application/lib/pages/page_element_creator/element_page_creator.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application/data/list_manager.dart';
-import 'package:flutter_application/widget/custom_button.dart';
+import 'package:flutter_application/image_picker_service.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ElementPgeCreator extends StatefulWidget {
@@ -97,37 +98,80 @@ class _ElementPgeCreatorState extends State<ElementPgeCreator> {
         ),
       ),
       body: Center(
-        child: Column(
-          children: [
-            _image != null
-                ? Image.file(
-                    _image!,
-                    height: 400,
-                  )
-                : Image.network(
-                    'https://i.ytimg.com/vi/3xlREA-SL_k/maxresdefault.jpg'),
-            CustomButton(
-              title: 'Pick from camera',
-              icon: Icons.camera,
-              onClick: () => _getImage(ImageSource.camera),
-            ),
-            CustomButton(
-                title: 'Pick from gallery',
-                icon: Icons.image_outlined,
-                onClick: () => _getImage(ImageSource.gallery)),
-            CustomButton(
-                title: 'image cropper',
-                icon: Icons.image_outlined,
-                onClick: () => ()), //_croppImage
-            CustomButton(
-              title: "Save element",
-              icon: Icons.save,
-              onClick: () {
-                widget.addElementCallback(Image.file(_image!));
-              },
-            )
-          ],
-        ),
+        child: _image != null
+            ? Image.file(
+                _image!,
+                height: 400,
+              )
+            : Image.network(
+                'https://i.ytimg.com/vi/3xlREA-SL_k/maxresdefault.jpg'),
+        // child: Column(
+        //   children: [
+        //     _image != null
+        //         ? Image.file(
+        //             _image!,
+        //             height: 400,
+        //           )
+        //         : Image.network(
+        //             'https://i.ytimg.com/vi/3xlREA-SL_k/maxresdefault.jpg'),
+        //     CustomButton(
+        //       title: 'Pick from camera',
+        //       icon: Icons.camera,
+        //       onClick: () => _getImage(ImageSource.camera),
+        //     ),
+        //     CustomButton(
+        //         title: 'Pick from gallery',
+        //         icon: Icons.image_outlined,
+        //         onClick: () => _getImage(ImageSource.gallery)),
+        //     CustomButton(
+        //         title: 'image cropper',
+        //         icon: Icons.image_outlined,
+        //         onClick: () async => {
+        //               ImagePickerService().pickCropImage(
+        //                 cropAspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+        //                 imageSource: ImageSource.camera,
+        //               )
+        //             }), //_croppImage
+        //     CustomButton(
+        //       title: "Save element",
+        //       icon: Icons.save,
+        //       onClick: () {
+        //         widget.addElementCallback(Image.file(_image!));
+        //       },
+        //     )
+        //   ],
+        // ),
+      ),
+      floatingActionButton: Column(
+        // padding: EdgeInsets.only(top: 10, bottom: 10),
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _getImage(ImageSource.camera),
+            child: Icon(Icons.camera),
+          ),
+          FloatingActionButton(
+            onPressed: () => _getImage(ImageSource.gallery),
+            child: Icon(Icons.photo_library_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: () async => {
+              ImagePickerService().pickCropImage(
+                cropAspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+                imageSource: ImageSource.camera,
+              )
+            },
+            child: Icon(Icons.image),
+          ),
+          FloatingActionButton(
+            onPressed: () => {widget.addElementCallback(Image.file(_image!))},
+            child: Icon(Icons.save),
+          ),
+          FloatingActionButton(
+            onPressed: () => {widget.addElementCallback(Image.file(_image!))},
+            child: Icon(Icons.delete),
+          ),
+        ],
       ),
     );
   }
