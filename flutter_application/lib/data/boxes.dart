3eb3fc414d7463074cea1bcdd78b66e8de3f4c05
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-11-04 06:29:18 +0100

    working but why?

--- flutter_application/lib/data/boxes.dart
+++ flutter_application/lib/data/boxes.dart
@@ -5,5 +5,5 @@ import 'my_element.dart';
 class Boxes {
   static Box<MyElement> getMyElements() => Hive.box<MyElement>('myElementBox');
   static Box<MyElement> getCollection() => Hive.box<MyElement>('collectionBox');
-  static Matrix4 m4 = Matrix4(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  static Matrix4 m4 = Matrix4.identity();
 }
