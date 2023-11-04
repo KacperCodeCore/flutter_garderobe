a947a605e06e18fe95e2804c327f32767dad16ba
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-11-04 06:06:23 +0100

    .

--- flutter_application/lib/data/collection.dart
+++ flutter_application/lib/data/collection.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:uuid/uuid.dart';
-
-import '../pages/collection/draggable_widget.dart';
 
 part 'collection.g.dart';
 // part 'auto_generated.g.dart';
